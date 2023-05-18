4ab9817459476ef9be2232e0291381ac4d312bf7
Paul Flood <paul.flood@digital.cabinet-office.gov.uk>
2023-05-18 16:13:58 +0100

    Update Test.ts

--- deploy/scripts/src/test.ts
+++ deploy/scripts/src/test.ts
@@ -4,11 +4,18 @@ import { sleep } from 'k6'
 
 export const options: Options = {
   duration: '1m',
-  vus: 50,
+  vus: 5,
   thresholds: {
     http_req_duration: ['p(95)<500']
   }
 }
+
+sleep(1);
+
+const checkRes = check(res, {
+    'status is 200': (r) => r.status === 200,
+    'response body': (r) => r.body.indexOf('Collection of simple web-pages suitable for load testing') !== -1,
+});
 
 export default function (): void {
   http.get('https://test.k6.io')
