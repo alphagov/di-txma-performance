1c25f260dd36ec66d213d2772f33813081ae52df
Paul Flood <114575593+paulfloodgds@users.noreply.github.com>
2023-05-19 11:16:58 +0100

    TT2-636 Add test script with response validation test_page_validation.ts  (#4)
    * First Commit
    
    Update README.md
    
    * Update Test.ts
    
    * update test.ts to address lint issues
    
    * Add original test.ts
    
    * add test script - test_page_validation.ts
    
    * Fix lint issue

---
+++ deploy/scripts/src/test_page_validation.ts
@@ -0,0 +1,24 @@
+import http from 'k6/http'
+import { type Options } from 'k6/options'
+import { sleep } from 'k6'
+
+export const options: Options = {
+  duration: '1m',
+  vus: 5,
+  thresholds: {
+    http_req_duration: ['p(95)<500']
+  }
+}
+
+sleep(1)
+
+const checkRes = check(res, {
+  'status is 200': (r) => r.status === 200,
+  'response body': (r) => r.body.indexOf('Collection of simple web-pages suitable for load testing') !== -1
+})
+
+export default function (): void {
+  http.get('https://test.k6.io')
+  sleep(1)
+  checkRes()
+}
